590c1c44de46cab586ea9d96d75793a5078f0dfa
Roland Flanagan <flanamacca@gmail.com>
2013-04-17 22:53:16 +1000

    Update linux_setup.sh
    more updates (added apps)

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,17 +1,28 @@
-## Node.JS
+## DEVELOPMENT
+# Node.JS
 sudo apt-get install python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
 
-## MONGO DB
+# MONGO DB
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/10gen.list
 sudo apt-get update
 sudo apt-get install mongodb-10gen
 
-## NAS
+
+## DESKTOP
+# NAS
 sudo mkdir -p /media/NAS
 sudo echo '//192.168.1.176/NASData /media/NAS cifs user,uid=1000,rw,suid,credentials=/etc/credentials 0 0' > /etc/fstab
 sudo mount -a
 
+# APPS
+sudo apt-get install vlc
+sudo apt-get install docky
+sudo apt-get install synaptic
+sudo apt-get install unity-tweak-tool
+# CHROME
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
