50df5036313ba353b28e50ec107f97d86f1f2e6f
Roland Flanagan <flanamacca@gmail.com>
2013-04-17 23:16:26 +1000

    Update linux_setup.sh
    Fixing tail ends

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
+cd /tmp
 
 # DEVELOPMENT
+echo '=> DEVELOPMENT ENVIRONMENT';
+
 # Node.JS
+echo ' -- Installing node.js';
 sudo apt-get install python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
 
 # MONGO DB
+echo ' -- Installing MongoDB';
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/10gen.list
+sudo touch /etc/apt/sources.list.d/10gen.list
+sudo sh -c "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen'" >> /etc/apt/sources.list.d/10gen.list
 sudo apt-get update
 sudo apt-get install mongodb-10gen
 
 
 ## DESKTOP
+echo '=> DESKTOP ENVIRONMENT';
+
 # NAS
+echo ' -- Setting up NAS';
 sudo mkdir -p /media/NAS
-sudo echo '//192.168.1.176/NASData /media/NAS cifs user,uid=1000,rw,suid,credentials=/etc/credentials 0 0' > /etc/fstab
-sudo mount -a
+sudo sh -c "echo '//192.168.1.176/NASData /media/NAS cifs user,uid=1000,rw,suid,credentials=/etc/credentials 0 0'" >> /etc/fstab
+mount -a
 
 # APPS
+echo ' -- Setting up Apps';
 sudo apt-get install vlc
 sudo apt-get install docky
 sudo apt-get install synaptic
@@ -28,4 +38,4 @@ sudo apt-get install unity-tweak-tool
 
 # CHROME
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
